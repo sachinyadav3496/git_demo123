4d0847d8bdbabff3cb2c4cd943139a6082ae17ab
sachinyadav3496 <sachinyadav3496@gmail.com>
2019-10-22 18:42:42 +0530

    optimized fib.py using memorization

--- fib.py
+++ fib.py
@@ -2,16 +2,21 @@
 this file contains some custom modules like
     fib
 """
+data = { } 
 def fib(n): 
     """
     fib(n) --> returns nth term of fibanacii series
     """
+    if n in data : 
+        return data[n]
     if n == 1 : 
         return 0
     elif n == 2 : 
         return 1
     else  : 
-        return fib(n-1) + fib(n-2)
+        ans =  fib(n-1) + fib(n-2)
+        data[n] = ans
+        return ans 
 
 if __name__ == "__main__" : 
     nterms = int(input("Enter number of terms : "))
